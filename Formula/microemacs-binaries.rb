3acb86ade82b883d351f6f9dccc7405e0f8eabad
Steven Phillips <steve@asid.family>
2024-09-30 14:45:12 +0100

    Fixes to binaries & added macros Formula

--- Formula/microemacs-binaries.rb
+++ Formula/microemacs-binaries.rb
@@ -1,22 +1,26 @@
 
 class MicroemacsBinaries < Formula
-  desc "MicroEmacs 24 Text Editor GUI Version"
+  desc "MicroEmacs 24 Text Editor - Terminal & GUI Binaries"
   homepage "https://github.com/bjasspa/jasspa"
   version "20240903"
   URLPFX="https://github.com/bjasspa/jasspa/releases/download/me_#{version}/Jasspa_MicroEmacs_#{version}_bin_"
   if OS.mac?
+      SHRPTH="/opt/homebrew/share"
       if Hardware::CPU.arm?
-          # Code for Apple Silicon (M1, M2, etc.)
-          ZIPPFX="bin/macos14-apple64/"
-          url "#{URLPFX}macos_apple_binaries.zip"
-          sha256 "840E128556F9D772FCF88CBFFACA1A026C05041FD6B168C6ACF9DDA9B8259D3D"
+        # Code for Apple Silicon (M1, M2, etc.)
+        ZIPPFX="bin/macos14-apple64"
+        url "#{URLPFX}macos_apple_binaries.zip"
+        sha256 "840E128556F9D772FCF88CBFFACA1A026C05041FD6B168C6ACF9DDA9B8259D3D"
       elsif Hardware::CPU.intel?
-          ZIPPFX="bin/macos13-intel64/"
-          url "#{URLPFX}macos_intel_binaries.zip"
-          sha256 "F358F6CB3F738D7FD7AEF4C844F3A346A43DDFA06B0716D96772D899DE2F23A4"
+        ZIPPFX="bin/macos13-intel64"
+        url "#{URLPFX}macos_intel_binaries.zip"
+        sha256 "F358F6CB3F738D7FD7AEF4C844F3A346A43DDFA06B0716D96772D899DE2F23A4"
+      else
+        odie "Unexpected OS, not linux or macos!"
       end
   elsif OS.linux?
-      ZIPPFX="bin/linux5-intel64/"
+      SHRPTH="/home/linuxbrew/.linuxbrew/share"
+      ZIPPFX="bin/linux5-intel64"
       url "#{URLPFX}linux_binaries.zip"
       sha256 "22F436BE7E400BF56AAE3DC714494ED98413F243E02AED59DDB227F6BA424196"
   else
@@ -24,8 +28,14 @@ class MicroemacsBinaries < Formula
   end
 
   def install
+    require 'fileutils'
+    FileUtils.mkdir_p("#{share}/jasspa") unless Dir.exist?("#{share}/jasspa")
+    FileUtils.mkdir_p("#{share}/jasspa/macros") unless Dir.exist?("#{share}/jasspa/macros")
+    FileUtils.mkdir_p("#{share}/jasspa/spelling") unless Dir.exist?("#{share}/jasspa/spelling")
     libexec.install Dir["*"]
-    (bin/"mew").write_env_script "#{libexec}/#{ZIPPFX}mew", :MEINSTALLPATH => "/home/linuxbrew/.linuxbrew/share/jasspa/"
+    (bin/"mew").write_env_script "#{libexec}/#{ZIPPFX}/mew", :MEINSTALLPATH => "#{share}/jasspa/"
+    (bin/"mec").write_env_script "#{libexec}/#{ZIPPFX}/mec", :MEINSTALLPATH => "#{share}/jasspa/"
+    (bin/"tfs").write_env_script "#{libexec}/#{ZIPPFX}/tfs"
     puts "start Microemacs with: MEPATH=~/.config/jasspa:macros:/home/linuxbrew/.linuxbrew/share/jasspa/spelling mew"
     puts "on MacOS replace /home/linuxbrew/.linuxbrew with /opt/homebrew for M1 Macs or /usr/local for Intel Macs"
   end
@@ -38,6 +48,6 @@ class MicroemacsBinaries < Formula
   end
   
   test do
-    assert_match "MicroEmacs 24 - Date 2024/09/02 - linux", shell_output("#{bin}/mew -V")
+    assert_match "MicroEmacs 24 - Date 2024-09-03 - linux", shell_output("#{bin}/mec -V")
   end
 end
---
+++ Formula/microemacs-macros.rb
@@ -0,0 +1,25 @@
+
+class MicroemacsMacros < Formula
+  desc "MicroEmacs 24 Text Editor Macro files"
+  homepage "https://github.com/bjasspa/jasspa"
+  version "20240903"
+  url "https://github.com/bjasspa/jasspa/releases/download/me_#{version}/Jasspa_MicroEmacs_#{version}_macros.zip"
+  sha256 "F92036BF751C4C9A02C7763985DA36E0CF94A034F03A6702826E33ABCB767277"
+
+  def install
+    # Define the target directory
+    macrofolder = "#{share}/jasspa/macros"
+    # Create the directory if it does not exist
+    require 'fileutils'
+    FileUtils.mkdir_p(macrofolder) unless Dir.exist?(macrofolder)
+    # Example: List files in the buildpath
+    Dir.glob("#{buildpath}/macros/*").each do |file|
+        puts "Found file: #{file}"
+        cp "#{file}", macrofolder
+    end  
+    ### setting MEPATH for mews and mecs does not work, what could we do?
+    puts "start Microemacs with: MEPATH=~/.config/jasspa:/home/linuxbrew/.linuxbrew/share/jasspa/macros:/home/linuxbrew/.linuxbrew/share/jasspa/spelling mec (or mew)"
+    puts "on MacOS replace /home/linuxbrew/.linuxbrew with /opt/homebrew for M1 Macs or /usr/local for Intel Macs"
+  end
+
+end
