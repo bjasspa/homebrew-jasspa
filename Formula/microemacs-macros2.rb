ae29f4ac3c7773e40c5b6a4aec78926f06103819
Detlef Groth <75636+mittelmark@users.noreply.github.com>
2024-10-13 08:06:58 +0200

    adding bottle commands

---
+++ Formula/microemacs-macros2.rb
@@ -0,0 +1,27 @@
+# -!- ruby -!-
+class MicroemacsMacros2 < Formula
+  desc "Jasspa MicroEmacs Text Editor - Macro files"
+  homepage "https://github.com/bjasspa/jasspa"
+  version "20240903"
+  SHRPTH="#{HOMEBREW_PREFIX}/share"
+  url "https://github.com/bjasspa/jasspa/releases/download/me_20240903/Jasspa_MicroEmacs_20240903_macros.zip"
+  sha256 "F92036BF751C4C9A02C7763985DA36E0CF94A034F03A6702826E33ABCB767277"
+  bottle do
+      sha256 cellar: :any_skip_relocation, x86_64_linux: "F92036BF751C4C9A02C7763985DA36E0CF94A034F03A6702826E33ABCB767277"
+  end
+  def install
+    require 'fileutils'
+    FileUtils.mkdir_p("#{share}/jasspa/macros") unless Dir.exist?("#{share}/jasspa/macros")
+    FileUtils.mkdir_p("#{SHRPTH}/jasspa/macros") unless Dir.exist?("#{SHRPTH}/jasspa/macros")
+    Dir.glob("#{buildpath}/macros/*").each do |bfn|
+      if File.file?(bfn)
+        fnm = File.basename(bfn)
+        sfn = "#{share}/jasspa/macros/#{fnm}"
+        puts "Found file: macros/#{fnm}"
+        cp "#{bfn}","#{share}/jasspa/macros"
+        ln_sf sfn,"#{SHRPTH}/jasspa/macros/#{fnm}"
+      end
+    end
+  end
+
+end
